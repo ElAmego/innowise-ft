9a0da46fb012dddb031580ba7bf863e77071b0b7
Egor <el.amegoal@gmail.com>
2025-12-09 12:25:26 +0300

    11 | changes in chapter 2 part 2

--- src/main/java/by/egoramel/ft/entity/CustomIntArrayData.java
+++ src/main/java/by/egoramel/ft/entity/CustomIntArrayData.java
@@ -2,4 +2,20 @@ package by.egoramel.ft.entity;
 
 public record CustomIntArrayData(int max, int min, int avg, int sum) {
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CustomIntArrayData that = (CustomIntArrayData) o;
+        return max == that.max && min == that.min && avg == that.avg && sum == that.sum;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = max;
+        result = 31 * result + min;
+        result = 31 * result + avg;
+        result = 31 * result + sum;
+        return result;
+    }
 }
