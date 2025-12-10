f0d0f5612c3182cd9b1872bf186b818eeab80bbb
Egor <el.amegoal@gmail.com>
2025-12-10 09:41:58 +0300

    13 | changes in chapter 2 part 4

--- src/main/java/by/egoramel/ft/observer/CustomIntArrayObservable.java
+++ src/main/java/by/egoramel/ft/observer/CustomIntArrayObservable.java
@@ -1,10 +1,9 @@
 package by.egoramel.ft.observer;
 
 import by.egoramel.ft.entity.CustomIntArray;
-import by.egoramel.ft.exception.CustomIntArrayException;
 
 public interface CustomIntArrayObservable {
     void addCustomIntArrayObserver(final CustomIntArrayObserver customIntArrayObserver);
     void removeCustomIntArrayObserver();
-    void notifyCustomIntArrayObservers(final CustomIntArray customIntArray) throws CustomIntArrayException;
+    void notifyCustomIntArrayObservers(final CustomIntArray customIntArray);
 }
